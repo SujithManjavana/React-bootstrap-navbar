6f83ea78a35d3dfb876681786926724fca9e8734
SujithManjavana <sujithmanjavana@gmail.com>
2020-06-17 13:25:30 +0530

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+import React from 'react';
+
+const Footer = props => {
+    return (
+        <div className="main-footer">
+            <p>Some footer nonsense</p>
+            <a target="_blank" href="http://sujithmanjavana.com">&copy; Sujith S Manjavana </a>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+const MyNavbar = props =>{
+    return (
+        <Navbar style={{background:"orange"}} expand="sm">
+        <Navbar.Brand href="#home">Hello bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Products</Nav.Link>
+            <NavDropdown title="Categories" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Smart watches</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Mobiles</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Laptops</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Offers</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+}
+
+export default MyNavbar;
